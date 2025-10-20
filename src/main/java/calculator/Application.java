4f3a5832341ad3cd843d9a7284a36f17fce14f20
J031 <rbehddl13@gmail.com>
2025-10-20 21:20:40 +0900

    feat: 커스텀 구분자 인식 및 구분자 목록 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,11 +7,20 @@ public class Application {
         // TODO: 프로그램 구현
 
         String input = Console.readLine();
+        String delimiter = ",|:"; // 기본 구문자
 
         if(input.startsWith("//")){
-            System.out.println("커스텀 구분자 형식");
+            String[] splitInput = input.split("\\\\n"); // 커스텀구분자를 \n기준으로 나뉘어 구분자와 숫자입력을 분리
+            String delimiterSection = splitInput[0].substring(2);
+            String numberSection = splitInput[1];
+
+            for(int i=0; i<delimiterSection.length(); i++){
+                delimiter += "|"+delimiterSection.charAt(i);
+            }
+            System.out.println("커스텀구분자 형식, 구분자 목록 : " + delimiter);
+            System.out.println(numberSection);
         } else {
-            System.out.println("기본 구분자 형식");
+            System.out.println("기본 구분자 형식, 구분자 목록 : " + delimiter);
         }
     }
 }
