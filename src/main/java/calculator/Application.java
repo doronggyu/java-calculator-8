c287998ae346a3c75cf83cb6b0c7e82fb31cc891
J031 <rbehddl13@gmail.com>
2025-10-20 19:39:30 +0900

    feat : 입력처리 및 커스텀 구분자 판별 로직 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        String input = Console.readLine();
+
+        if(input.startsWith("//")){
+            System.out.println("커스텀 구분자 형식");
+        } else {
+            System.out.println("기본 구분자 형식");
+        }
     }
 }
