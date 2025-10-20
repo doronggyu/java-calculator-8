b8d817fc6a48e9ea146421dfb2591f85e868b112
J031 <rbehddl13@gmail.com>
2025-10-20 21:29:25 +0900

    feat: 문자열 입력값을 구분자로 분리하여 합산하는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,21 +6,31 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
         String input = Console.readLine();
         String delimiter = ",|:"; // 기본 구문자
+        String numberSection = input;
 
         if(input.startsWith("//")){
             String[] splitInput = input.split("\\\\n"); // 커스텀구분자를 \n기준으로 나뉘어 구분자와 숫자입력을 분리
             String delimiterSection = splitInput[0].substring(2);
-            String numberSection = splitInput[1];
+            numberSection = splitInput[1];
 
             for(int i=0; i<delimiterSection.length(); i++){
                 delimiter += "|"+delimiterSection.charAt(i);
             }
-            System.out.println("커스텀구분자 형식, 구분자 목록 : " + delimiter);
-            System.out.println(numberSection);
-        } else {
-            System.out.println("기본 구분자 형식, 구분자 목록 : " + delimiter);
+
         }
+
+        String[] numbers = numberSection.split(delimiter);
+        int sum = 0;
+
+        for(String numList : numbers){
+            int num = Integer.parseInt(numList);
+            sum += num;
+        }
+
+        System.out.println("결과 : " + sum);
     }
 }
